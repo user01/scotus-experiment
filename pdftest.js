cf4499ae9852a70e945ee6423369b8abb4aa21e1
Erik Langenborg <erik.langenborg@gmail.com>
2016-03-22 16:28:29 -0400

    Properly filter text

--- pdftest.js
+++ pdftest.js
@@ -24,76 +24,40 @@ const pdfParser = new PDFParser();
 
 pdfParser.on("pdfParser_dataError", errData => console.error('err', errData));
 pdfParser.on("pdfParser_dataReady", pdfData => {
-  debugger;
-  // var text = R.pipe(
-  //   R.prop('data'),
-  //   R.prop('Pages'),
-  //   R.addIndex(R.map)(R.prop('Texts')),
-  //   R.flatten,
-  //   R.map(R.prop('R')),
-  //   R.flatten,
-  //   R.map(R.prop('T')),
-  //   R.map(decodeURIComponent),
-  //   R.join(''))(pdfData);
-
 
   var text = R.pipe(
     R.prop('data'),
     R.prop('Pages'),
-    R.map((page) => {
-      var texts = R.pipe(
-        R.prop('Texts'),
-        R.map((elm) => {
-          return objectAssign({}, elm, { line: Math.floor(elm.y * 10) });
-        }),
-        R.groupBy(R.prop('line')),
-        R.values,
-        //now an array of arr of objs with R props
-        R.map(R.pipe(
-          R.map(R.prop('R')),
-          R.flatten,
-          R.map(R.prop('T')),
-          R.map(decodeURIComponent),
-          R.join('')
-        )),
-        R.addIndex(R.map)((line, idx) => {
-          // lines need to have the string of the idx+1 leading.
-          // this number gets stripped off
-          // if it's missing the line is turned into a false
-          const targetStr = '' + (idx + 1);
-          const index = line.indexOf(targetStr);
-          return (index < 0) ? false : R.splitAt(targetStr.length)(line)[1];
-        }),
-        R.filter(R.identity)
+    R.map(R.pipe(
+      R.prop('Texts'),
+      R.map((elm) => {
+        return objectAssign({}, elm, { line: Math.floor(elm.y * 10) });
+      }),
+      R.groupBy(R.prop('line')),
+      R.values,
+      //now an array of arr of objs with R props
+      R.map(R.pipe(
+        R.map(R.prop('R')),
+        R.flatten,
+        R.map(R.prop('T')),
+        R.map(decodeURIComponent),
+        R.join('')
+      )),
+      R.addIndex(R.map)((line, idx) => {
+        // lines need to have the string of the idx+1 leading.
+        // this number gets stripped off
+        // if it's missing the line is turned into a false
+        const targetStr = '' + (idx + 1);
+        const index = line.indexOf(targetStr);
+        return (index != 0) ? false : R.splitAt(targetStr.length)(line)[1];
+      }),
+      R.filter(R.identity)
 
-      )(page);
-      debugger;
-
-    })
-
-    // R.flatten,
-    // R.map(R.prop('R')),
-    // R.flatten,
-    // R.map(R.prop('T')),
-    // R.map(decodeURIComponent),
-    // R.join('')
-
+    )),
+    R.flatten
   )(pdfData);
 
-
-  // var text = R.pipe(
-  //   R.map(R.prop('Texts')),
-  //   R.flatten,
-  //   R.map(R.prop('R')),
-  //   R.flatten,
-  //   R.map(R.prop('T')),
-  //   R.map(decodeURIComponent),
-  //   R.concat
-  // )(pdfData);
   console.log(text);
-  // console.log(pdfData);
-  // console.log(JSON.stringify(pdfData));
-  // fs.writeFile("./F1040EZ.json", JSON.stringify(pdfData));
 });
 
 pdfParser.loadPDF(pathToPdf);
