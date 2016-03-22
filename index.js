b7f30f6674e70a5d68d523dd12f80c96ccbd9460
Erik Langenborg <erik.langenborg@gmail.com>
2016-03-22 13:19:00 -0400

    testing

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
 
 
+var Promise = require('bluebird');
 var jsdom = require("jsdom");
 var R = require('ramda');
 var http = require('http');
-var fs = require('fs');
+var fs = Promise.promisifyAll(require("fs"), { suffix: "Async" });
 var path = require('path');
-var Promise = require('bluebird');
+var moment = require('moment');
 
-var years = R.range(2000, 2016);
-var root = "http://www.supremecourt.gov/oral_arguments/argument_transcript/";
+const years = R.range(2000, 2016);
+const root = "http://www.supremecourt.gov/oral_arguments/argument_transcript/";
 
 
 const rowWork = (window, row) => {
@@ -34,24 +35,25 @@ const rowWork = (window, row) => {
   // console.log(year, code, title, dateArgued, pathToPDF, fullFilename);
 
 
-  fs.stat(fullFilename, function(err, stat) {
-    if (err == null) {
-      console.log(fullFilename + ' already exists');
-    } else if (err.code == 'ENOENT') {
+  // return fs.statAsync(fullFilename).then((stat) => {
+  //   console.log(fullFilename + ' already exists');
+  // }).catch((err) => {
+  //   // good case
+  //   if (err.code == 'ENOENT') {
 
-      var file = fs.createWriteStream(fullFilename);
-      var request = http.get(pathToPDF, function(response) {
-        response.pipe(file);
-        console.log('Wrote ' + fullFilename);
-      });
+  //     var file = fs.createWriteStream(fullFilename);
+  //     var request = http.get(pathToPDF, function(response) {
+  //       response.pipe(file);
+  //       console.log('Wrote ' + fullFilename);
+  //     });
 
-    } else {
-      console.log('Some other error: ', err.code);
-    }
-  });
+  //   } else {
+  //     console.log('Some other error: ', err.code);
+  //   }
+  // });
 
 
-  const date = moment(dateArgued, "MM/DD/YYYY");
+  const date = moment(dateArgued, "MM/DD/YYYY").toISOString();
 
   return {
     code,
@@ -65,25 +67,33 @@ const rowWork = (window, row) => {
 
 
 
+const getFilesFromYear = (year) => {
+  return new Promise(function(resolve, reject) {
 
-
-
-R.forEach((year) => {
-  var path = root + year;
-
-  jsdom.env(
-    path,
-    ["http://code.jquery.com/jquery.js"],
-    function(err, window) {
-      var rows = window.$("tr");
-      var datas = [];
-      for (var i = 0; i < rows.length; i++) {
-        datas.push(rowWork(window, rows[i]));
+    var path = root + year;
+    jsdom.env(
+      path,
+      ["http://code.jquery.com/jquery.js"],
+      function(err, window) {
+        var rows = window.$("tr");
+        // resolve(rows.length);
+        var datas = [];
+        for (var i = 0; i < rows.length; i++) {
+          datas.push(rowWork(window, rows[i]));
+        }
+        // var cleanedData = R.filter(R.identity, datas);
+        resolve(Promise.all(datas, { concurreny: 2 }));
       }
-      var cleanedData = R.filter(R.identity,datas);
-    }
-  );
-}, years);
+    );
+
+  });
+}
+
+
+Promise.map(years, getFilesFromYear, { concurreny: 2 }).then((results) => {
+  console.log(results);
+});
+
 
 
 
