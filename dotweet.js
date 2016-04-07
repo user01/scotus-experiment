31b64fb9f035d36193a400b4bcb478dc5e531e94
Erik Langenborg <erik.langenborg@gmail.com>
2016-04-07 19:35:26 -0400

    More picking tweet code. Still not sure if this is the best path

--- dotweet.js
+++ dotweet.js
@@ -2,6 +2,7 @@ const Datastore = require('nedb');
 const path = require('path');
 const Promise = require('bluebird');
 const R = require('ramda');
+const Chance = require('chance');
 
 const fs = Promise.promisifyAll(require("fs"), { suffix: "Async" });
 const db = new Datastore({ filename: path.join(__dirname, 'tweets.db'), autoload: true });
@@ -52,34 +53,69 @@ const writeDocToDb = (doc) => {
 };
 const handleMissingName = (filename) => {
   return readJson(filename)
-    .then(transformJsonTweetIntoZeroedDoc)
-    // .then((doc) => {
-    //   console.log('heard doc', doc);
-    // })
-    .then(writeDocToDb);
+    .then(transformJsonTweetIntoZeroedDoc);
+}
+const pickNextTweet = (state) => {
+  const chance = new Chance(state.count);
+  const justiceSpeaking = state.count % 2 == 0;
+  const validTweeters = R.pipe(R.filter(
+    R.pipe(
+      R.prop('isJustice'),
+      justiceSpeaking ? R.identity : R.not
+    )),
+    R.filter((item) => {
+      return item.currentTweet < item.totalTweets
+    })
+  )(state.tweeters);
+  if (validTweeters < 1) {
+    return 000;
+  }
+
+  const pickedIndex = chance.natural({ min: 0, max: validTweeters.length });
+  const filename = validTweeters[pickedIndex].filename;
+
+  return {
+    filename,
+    tweet,
+    newState
+  }
 }
 
-db.find({ filename: { $exists: true } }).exec((err, dbKnownFiles) => {
+// {
+//   count: 0,
+//   tweeters: [{name,filename,currentTweet,totalTweets,isJustice}]
+// }
+
+db.find({ count: { $exists: true } }).exec((err, states) => {
+  const state = states.length > 0 ? states[0] : { count: 0, tweeters: [] };
+  console.log(state);
 
   fs.readdirAsync(tweetRoot)
     .then(filterToJsonFiles)
     .then((fsFiles) => {
       console.log('fs seen files', fsFiles.length)
-      const dbKnownFilesnames = R.map(R.prop('filename'))(dbKnownFiles);
+      const dbKnownFilesnames = R.map(R.prop('filename'), state.tweeters);
       console.log('Known ', dbKnownFilesnames.length);
       const missingFiles = R.filter(R.pipe(
         R.flip(R.contains)(dbKnownFilesnames),
         R.not
       ))(fsFiles);
       console.log('Missing:', missingFiles.length);
+
       Promise.map(missingFiles, handleMissingName)
         .then((addedFiles) => {
           console.log("All done with ", addedFiles);
-          const knownFiles = R.concat(addedFiles, dbKnownFiles);
+          const tweeters = R.concat(addedFiles, state.tweeters);
+          console.log(tweeters);
+          const newState = {
+            count: state.count,
+            tweeters
+          };
+          return newState;
 
-          db.find({ index: { $exists: true } }).exec((err, dbKnownFiles) => {
-          });
+          // db.find({ index: { $exists: true } }).exec((err, dbKnownFiles) => {
         })
+        .then()
     });
 
 });
