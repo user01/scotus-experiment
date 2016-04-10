8ecf9de57acb89b6383f8f4b1f42cf66043da3b7
Erik Langenborg <erik.langenborg@gmail.com>
2016-04-10 19:52:13 -0400

    Added logic for how to run the tweets

--- dotweet.js
+++ dotweet.js
@@ -3,12 +3,15 @@ const path = require('path');
 const Promise = require('bluebird');
 const R = require('ramda');
 const Chance = require('chance');
+const Moment = require('moment');
+const MomentTz = require('moment-timezone');
 
 const fs = Promise.promisifyAll(require("fs"), { suffix: "Async" });
 const db = new Datastore({ filename: path.join(__dirname, 'tweets.db'), autoload: true });
 
 const NO_TWEETS_REMAIN = "NO_TWEETS_REMAIN";
 const tweetRoot = path.join(__dirname, 'tweets');
+const TIME_FRAME_MINUTES = 10;
 
 const filterToJsonFiles = (list) => {
   const match = /.*\.json$/;
@@ -79,7 +82,7 @@ const handleMissingName = (filename) => {
   return readJson(filename)
     .then(transformJsonTweetIntoZeroedDoc);
 }
-const pickNextTweet = (state) => {
+const pickNextTweetIndex = (state) => {
   const justiceSpeaking = state.count % 2 == 0;
   const validTweeters = R.pipe(R.filter(
     R.pipe(
@@ -133,6 +136,11 @@ const getPickedTweet = (payload) => {
     })
 }
 
+const tweet = (content) => {
+  console.log('Trying to tweet: ', content);
+  return Promise.resolve(content);
+}
+
 
 const generateMissingFiles = (state, fsFiles) => {
   const dbKnownFilesnames = R.map(R.prop('filename'), state.tweeters);
@@ -158,20 +166,129 @@ const ensureStateMatchesFiles = (state) => {
 };
 
 const joinTweetingAndStateWriting = (payload) => {
-  const tweetingPromise = Promise.resolve(payload.tweet);
+  const tweetingPromise = tweet(payload.tweet);
   const writingStatePromise = setDocInDb(payload.newState);
   return Promise.join(tweetingPromise, writingStatePromise);
 }
 
 
+const doTweet = () => {
 
-findOnInDb({ count: { $exists: true } })
-  .then(handleEmptyState)
-  .then(ensureStateMatchesFiles)
-  .then(pickNextTweet)
-  .then(getPickedTweet)
-  .then(joinTweetingAndStateWriting)
-  .then((res) => {
-    //all done with tweet pass
-    console.log('All done!', res[0]);
-  });
+  return findOnInDb({ count: { $exists: true } })
+    .then(handleEmptyState)
+    .then(ensureStateMatchesFiles)
+    .then(pickNextTweetIndex)
+    .then(getPickedTweet)
+    .then(joinTweetingAndStateWriting)
+  // .then((res) => {
+  //   //all done with tweet pass
+  //   // if (res[1].count % 100 == 0)
+  //   //   console.log(res[1].count, res[0]);
+  //   // doTweet();
+  // })
+}
+
+// doTweet();
+
+// const currentTime = Moment("2016-04-10T22:30:30.015Z");
+const cleanMoment = (targetMoment) => {
+  return MomentTz(targetMoment, "America/New_York").set({ 'second': 0, 'millisecond': 0 });
+}
+const shouldDoTweet = (moment) => {
+  const currentTime = cleanMoment(moment);
+  if (currentTime.day() < 1 && currentTime.day() > 3) return false;
+  if (currentTime.hour() < 10 || currentTime.hour() >= 12) return false;
+  return true;
+}
+
+const shouldBeClerkOpeningTweet = (currentTime) => {
+  if (currentTime.hour() !== 10) return false;
+  if (currentTime.minute() < TIME_FRAME_MINUTES) return true;
+  return false;
+}
+const shouldBeClerkClosingTweet = (currentTime) => {
+  if (currentTime.hour() == 11) {
+    if (currentTime.minute() >= 60 - TIME_FRAME_MINUTES) return true;
+  }
+  return false;
+}
+
+const CLERK_HEADER = 'Clerk: ';
+const CLERK_OPENERS = [
+  "Starting to record. I hope this tinnitus abates or I'll never hear the arguments",
+  "Opening the transcript. I hope the Justices stop mumbling.",
+  "Word Processor. New Document. Oops, the they've started already.",
+  "That loud concert last night really hit my ears hard. Mind if I sprinkle in a few lyrics? They'll be relevant.",
+  "Another day, another dollar.",
+  "I'm in the mood for an easy day. What say everyone speaks really slowly.",
+  "In this building. Again. It's stately enough, but can't we do this outside for once? My feed says it's a nice day.",
+  "My aren't those robes slimming. Wait, hold on, I'm writing all this down."
+];
+const CLERK_CLOSERS = [
+  "Phew. All done for the day. Who's up for a party?",
+  "That's all folks.",
+  "Well, I think we're all full up on legal history today.",
+  "Strict constructionism certainly won today. Whatever that is.",
+  "Saw some judical activism today. You should see how active they are. When they're hungry for lunch.",
+  "I live-tweeted this on my personal account. Should I have checked with the court first?",
+  "Did you see that guy in the thrid row? I have no idea how he got a tuba into the gallery."
+]
+const openerTweet = (currentTime) => {
+  return tweet(`${CLERK_HEADER}${new Chance(currentTime.unix()).pickone(CLERK_OPENERS)}`);
+}
+const closerTweet = (currentTime) => {
+  return tweet(`${CLERK_HEADER}${new Chance(currentTime.unix()).pickone(CLERK_CLOSERS)}`);
+}
+
+
+
+const go = (moment) => {
+  const currentTime = cleanMoment(moment);
+  if (!shouldDoTweet(currentTime)) return Promise.resolve(false);
+  if (shouldBeClerkOpeningTweet(currentTime)) {
+    return openerTweet(currentTime);
+  } else if (shouldBeClerkClosingTweet(currentTime)) {
+    return closerTweet(currentTime);
+  }
+  return doTweet();
+}
+
+
+// const test = (m) => {
+//   const mm = Moment(m);
+//   console.log(`For ${mm.format("dddd, MMMM Do YYYY, h:mm:ss a")} ${shouldDoTweet(mm) ? 'will' : "won't"} do tweet ${shouldBeClerkOpeningTweet(mm) ? "And Is Clerk Opener" : ""} ${shouldBeClerkClosingTweet(mm) ? "And Is Clerk Closer" : ""}`);
+//   // go(mm).then((res) => console.log(`result ${res}`));
+// }
+// Promise.resolve(false).then(res => console.warn(res));
+// test("2016-04-12T13:50:30.015Z");
+// test("2016-04-12T14:00:30.015Z");
+// test("2016-04-12T14:10:30.015Z");
+// test("2016-04-12T14:20:30.015Z");
+// test("2016-04-12T14:30:30.015Z");
+// test("2016-04-12T14:40:30.015Z");
+// test("2016-04-12T14:50:30.015Z");
+// test("2016-04-12T15:00:30.015Z");
+// test("2016-04-12T15:10:30.015Z");
+// test("2016-04-12T15:20:30.015Z");
+// test("2016-04-12T15:30:30.015Z");
+// test("2016-04-12T15:40:30.015Z");
+// test("2016-04-12T15:50:30.015Z");
+// test("2016-04-12T16:00:30.015Z");
+// test("2016-04-12T16:10:30.015Z");
+// test("2016-04-12T16:20:30.015Z");
+
+
+// test("2016-04-10T22:30:30.015Z");
+// test("2016-04-12T22:30:30.015Z");
+// test("2016-04-12T10:30:30.015Z");
+// test("2016-04-14T22:30:30.015Z");
+// test("2016-04-16T22:30:30.015Z");
+// test("2016-04-18T22:30:30.015Z");
+
+const currentTime = Moment();
+go(currentTime)
+  .then(() => console.log(`Completed run ${currentTime.format("dddd, MMMM Do YYYY, h:mm:ss a")}`))
+  .catch((err) => {
+    console.warn(`ERROR at ${currentTime.format("dddd, MMMM Do YYYY, h:mm:ss a")}`);
+    console.warn(err);
+  })
